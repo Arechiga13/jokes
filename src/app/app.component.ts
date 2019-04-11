90096f31552af130f283319ef7fc6eef88af7810
Manuel Arechiga <manuel.arechiga@amplemind.com>
2019-04-11 15:19:05 -0500

    Add PWA function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,28 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 
 import { DataService } from './data.service';
+import { PwaService } from './pwa.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'jokes';
-  update: boolean = false;
   joke: any;
 
-  constructor(updates: SwUpdate, private data: DataService) {
+  constructor(updates: SwUpdate, private data: DataService, public Pwa: PwaService) {
     updates.available.subscribe(event => {
-      // this.update = true;
       updates.activateUpdate().then(() => document.location.reload());
-    })
+    });
   }
 
   ngOnInit() {
     this.data.gimmeJokes().subscribe(res => {
       this.joke = res;
-    })
+    });
   }
 
 }
